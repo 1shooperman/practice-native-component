1d3d5e36e5072889f99760526211f4e77ee00220
Brandon Shoop <brandon.shoop@boats.com>
2021-08-19 16:35:10 -0400

    Created the ObjC version of the file.

---
+++ ios/MyCustomView.h
@@ -0,0 +1,16 @@
+//
+//  MyCustomView.h
+//  AwesomeProject
+//
+//  Created by Brandon  Shoop on 8/19/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyCustomView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/MyCustomView.m
@@ -0,0 +1,20 @@
+//
+//  MyCustomView.m
+//  AwesomeProject
+//
+//  Created by Brandon  Shoop on 8/19/21.
+//
+
+#import "MyCustomView.h"
+
+@implementation MyCustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
