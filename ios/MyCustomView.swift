2f2a9b90e11c237266511715dfc8a1e0d50cbd2b
Brandon Shoop <brandon.shoop@boats.com>
2021-08-19 16:32:31 -0400

    [main] Opened in XCode, Added MyCustomView per tutorial.
    Opening in XCode added the xcshareddate plist file. The rest were created with the new file.

---
+++ ios/MyCustomView.swift
@@ -0,0 +1,20 @@
+//
+//  MyCustomView.swift
+//  AwesomeProject
+//
+//  Created by Brandon  Shoop on 8/19/21.
+//
+
+import UIKit
+
+class MyCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
